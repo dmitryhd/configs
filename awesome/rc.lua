b1f0504d827bdfcd9f705621bdb92573a45a16b4
Dmitriy Khodakov <dmitryhd@gmail.com>
2014-10-30 16:31:51 +0300

    1

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -78,7 +78,7 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, }, s, layouts[1])
+    tags[s] = awful.tag({ 'browser', 'vim', 'console', 'other' }, s, layouts[1])
 end
 -- }}}
 
