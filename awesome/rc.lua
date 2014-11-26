4d111eb6bcd499aef9f64a7fba765cafa1871d56
Dmitriy Khodakov <dmitryhd@gmail.com>
2014-11-26 07:54:29 +0300

    fixed upload bug, awesome tags addes

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -59,15 +59,15 @@ layouts =
 {
     awful.layout.suit.floating,
     awful.layout.suit.tile,
+    awful.layout.suit.max,
     --awful.layout.suit.tile.left,
     --awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
     --awful.layout.suit.fair,
     --awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
+    --awful.layout.suit.spiral,
     --awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
+    --awful.layout.suit.max.fullscreen,
     --awful.layout.suit.magnifier
 }
 -- }}}
@@ -76,8 +76,12 @@ layouts =
 -- Define a tag table which hold all screen tags.
 tags = {}
 for s = 1, screen.count() do
-    -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 'browser', 'vim', 'console', 'files', 'other01', 'other02' }, s, layouts[2])
+  -- Each screen has its own tag table.
+  --  tags[s] = awful.tag({ 'browser', 'vim', 'console', 'files', 'other01', 'other02' }, s, layouts[2])
+  tags[s] = awful.tag({ 'browser', 'vim', 'console', 'files', 
+                        'VM', 'remote', 'other' }, s,
+                      { layouts[1], layouts[2], layouts[2], layouts[1],
+                        layouts[1], layouts[1], layouts[1]})
 end
 -- }}}
 
@@ -324,8 +328,12 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1)      end),
     awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1)         end),
     awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
-    awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
-    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
+--    awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
+--    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
+    awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1)
+                                              naughty.notify({ title = 'Layout', text = awful.layout.getname(), timeout = 3 }) end),
+    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1)
+                                              naughty.notify({ title = 'Layout', text = awful.layout.getname(), timeout = 3 }) end),
 
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
--- configs_upload.sh
+++ configs_upload.sh
@@ -8,7 +8,6 @@ $copy_command ~/.vimrc .
 $copy_command ~/.vimrc_functions .
 $copy_command ~/.zshrc .
 $copy_command ~/.oh-my-zsh/themes/dimert2.zsh-theme .
-$copy_command ~/.config/awesome ./awesome
 $copy_command ~/.vim/ .
 $copy_command ~/.config/awesome .
 $copy_command ~/.conkyrc .
